c1102ed656002d3aa555b1f604a617968401e3bd
inouenaoto0102 <inouenaoto0102@gmail.com>
2018-04-28 09:24:19 +0900

    ライブラリ導入

---
+++ app/src/main/java/camera/library/android/andpad/p/waternote/MainActivity.kt
@@ -0,0 +1,12 @@
+package camera.library.android.andpad.p.waternote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
